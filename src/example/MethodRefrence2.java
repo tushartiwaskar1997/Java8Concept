27f756cb722f83125e79d730dbea392a9bf83317
tushartiwaskar1997 <tushartiwaskar.svpcet@gmail.com>
2024-09-04 21:23:22 +0530

    Method Refrence normal method !!

--- src/example/MethodRefrence2.java
+++ src/example/MethodRefrence2.java
@@ -7,14 +7,24 @@ public class MethodRefrence2 {
 			System.out.println("This is comgin from the chiild !!! " + i);
 		}
 	}
+	
+	public static void  test2() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("This is comgin from the chiild@2 !!! " + i);
+		}
+	}
 
 	public static void main(String[] args) {
 
-		Runnable runable = MethodRefrence2::test;
+		Runnable runable = MethodRefrence2::test; // this is good with static method
 		Thread th = new Thread(runable);
 		th.start();
 		for (int i = 0; i < 5; i++) {
 			System.out.println("This is comgin from the parent  !!! " + i);
 		}
+		
+		MethodRefrence2 m =  new MethodRefrence2();
+		Runnable runobj2  =  m::test2();
+		
 	}
 }
